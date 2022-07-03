61abc89c62d227356d2c2ebb350ed395eb36c844
waleed smadi <waleed0smadi@outlook.com>
2022-07-03 13:57:11 +0300

    JS: finish colors function

--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@
 // select elements
 const mainImage = document.querySelector("#main-image");
 const icons = Array.from(document.querySelectorAll(".icons img"));
-
+const headerElement = document.querySelector("header");
 
 // function to change main image
 function phones(phone){
     mainImage.src = phone;
 }
+
+
+// to change header's background color
+// when click on bottom icons 
+function colors(color){
+    headerElement.style.background = color;
+} 
 
 
 // add events to bottom icons
@@ -17,3 +24,5 @@ icons.forEach(icon => {
         phones(this.src);
     });
 });
+
+
